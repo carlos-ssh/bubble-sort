4b981e12799ae34afeb8b51b83bd0bbc64c16596
John Elvis Durán <jelduran@gmail.com>
2020-08-11 17:07:47 -0500

    bubble_sort_by yield:
    bubble_sort_by method implemented using yield method.

--- Sort.rb
+++ Sort.rb
@@ -1,37 +1,37 @@
 class Sort
   def bubble_sort list
     rightmost = list.length - 1
-      until rightmost < 1 do
-        ordered_flag = true
-        left = 0
-        until left >= rightmost do
-          if list[left] > list[left+1]
-            list[left], list[left+1] = list[left+1], list[left]
+    until rightmost < 1 do
+      ordered_flag = true
+      left = 0
+      until left >= rightmost do
+        if list[left] > list[left+1]
+          list[left], list[left+1] = list[left+1], list[left]
             ordered_flag = false
-          end
-          left += 1
         end
-        return list if ordered_flag
-        rightmost -= 1
+        left += 1
       end
-      return list
+      return list if ordered_flag
+      rightmost -= 1
+    end
+    return list
   end
 
   def bubble_sort_by list
     rightmost = list.length - 1
-      until rightmost < 1 do
-        ordered_flag = true
-        left = 0
-        until left >= rightmost do
-          if list[left] > list[left+1]
-            list[left], list[left+1] = list[left+1], list[left]
-            ordered_flag = false
-          end
-          left += 1
+    until rightmost < 1 do
+      ordered_flag = true
+      left = 0
+      until left >= rightmost do
+        if yield(list[left], list[left+1]) > 0
+          list[left] , list[left+1] = list[left+1] , list[left]
+          ordered_flag = false
         end
-        return list if ordered_flag
-        rightmost -= 1
+        left+=1
       end
-      return list
+      return list if ordered_flag
+      rightmost -= 1
+    end
+    return list
   end
 end
