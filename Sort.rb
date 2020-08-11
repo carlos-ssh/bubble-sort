72db16fc312f388f55fb1232f99c93098095502b
Carlos Robles <carlos.anezeh@gmail.com>
2020-08-11 16:07:30 -0500

    bubble_sort_by method:
    bubble_sort_by method created based on bubble_sort method.

--- Sort.rb
+++ Sort.rb
@@ -16,4 +16,22 @@ class Sort
       end
       return list
   end
+
+  def bubble_sort_by list
+    rightmost = list.length - 1
+      until rightmost < 1 do
+        ordered_flag = true
+        left = 0
+        until left >= rightmost do
+          if list[left] > list[left+1]
+            list[left], list[left+1] = list[left+1], list[left]
+            ordered_flag = false
+          end
+          left += 1
+        end
+        return list if ordered_flag
+        rightmost -= 1
+      end
+      return list
+  end
 end
