f82b91ff339182f858dc34a8d3da95ee546339da
Carlos Robles <carlos.anezeh@gmail.com>
2020-08-11 17:57:22 -0500

    Changed the name of the class

--- Sort.rb
+++ Sort.rb
@@ -1,4 +1,4 @@
-class sort
+class mi_lista
   def bubble_sort(list)
     rightmost = list.length - 1
     until rightmost < 1
@@ -23,7 +23,7 @@ class sort
     rightmost = list.length - 1
     until rightmost < 1
       ordered_flag = true
-      
+
       left = 0
       until left >= rightmost
         if yield(list[left], list[left + 1]).positive?
