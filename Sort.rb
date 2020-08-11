db1a2a8c34514d23ccefcd9061f367b6f3c6bb29
John Elvis Durán <jelduran@gmail.com>
2020-08-11 15:26:24 -0500

    Variable ordered_flag:
    This is a variable flag to check if the array is already ordered, it is set as true assuming that array is ordered from the begining.

--- Sort.rb
+++ Sort.rb
@@ -1,8 +1,9 @@
 class Sort
-    def bubble_sort
-      rightmost = list.length - 1
-      until rightmost < 1 do 
-          rightmost -= 1
+  def bubble_sort
+    rightmost = list.length - 1
+      until rightmost < 1 do
+        ordered_flag = true
+        rightmost -= 1
       end
-    end
+  end
 end
