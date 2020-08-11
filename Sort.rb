c0f46ebc9bb7e907c2a48902629f9a42bfab1389
Carlos Robles <carlos.anezeh@gmail.com>
2020-08-11 17:52:51 -0500

    Fixing issues with linter

--- Sort.rb
+++ Sort.rb
@@ -1,13 +1,15 @@
-class main_sort
+class sort
   def bubble_sort(list)
     rightmost = list.length - 1
     until rightmost < 1
       ordered_flag = true
+
       left = 0
       until left >= rightmost
         if list[left] > list[left + 1]
           list[left], list[left + 1] = list[left + 1], list[left]
             ordered_flag = false
+
         end
         left += 1
       end
@@ -21,11 +23,13 @@ class main_sort
     rightmost = list.length - 1
     until rightmost < 1
       ordered_flag = true
+      
       left = 0
       until left >= rightmost
         if yield(list[left], list[left + 1]).positive?
           list[left], list[left + 1] = list[left + 1], list[left]
           ordered_flag = false
+
         end
         left+=1
       end
