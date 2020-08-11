5a9392ae39d115356ea85ed74a10c8df1d699bee
Carlos Robles <carlos.anezeh@gmail.com>
2020-08-11 15:32:35 -0500

    Internal loop:
    This loop its going again through the array from the begining to the last unordered element.

--- Sort.rb
+++ Sort.rb
@@ -3,6 +3,10 @@ class Sort
     rightmost = list.length - 1
       until rightmost < 1 do
         ordered_flag = true
+        left = 0
+        until left >= rightmost do
+            
+        end
         rightmost -= 1
       end
   end
